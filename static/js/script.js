9517f2cba4620ada42e79f62a510e42fd531c5c4
DiegoG08 <diegoguadarrama08@gmail.com>
2020-09-28 18:01:28 -0700

    updated BlackJack

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
+
+
 // Challenge 1: Your Age in Days 
-
-
 ageInDays=()=> {
     var birthYear =prompt('What year were you born?'); 
     var ageInDays = (birthYear-2020)*365; 
@@ -15,10 +15,27 @@ reset=()=> {
     document.getElementById('ageInDays').remove();
 }
 
+// Challenge 2: Cat Meme Generator
 generateCat=()=>{
     var image = document.createElement('img');
     var div = document.getElementById('flex-cat-gen');
     image.src ="http://thecatapi.com/api/images/get?format=src&type=gif&size-tiny";
     div.appendChild(image);
+ 
+}
+
+// Challenge 3: Rock Paper Scrissors
+
+rpsGame = (yourChoice)=>{
+    console.log(yourChoice.id);
+    var humanChoice, botChoice; 
+    //humanChoice= yourChoice.id;
+    // botChoice = 
+
+    // results = decideWinner(humanChoice, botChoice);
+    // message = finalMessage(results);
+    rps
+    
+
 
 }
